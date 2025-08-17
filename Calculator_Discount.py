78a1f5b593bfdcae6e5ae147315f558a65e248d0
IanWachcode <ianwachira90@gmail.com>
2025-08-17 16:19:57 +0300

    assignment final

--- Calculator_Discount.py
+++ Calculator_Discount.py
@@ -5,3 +5,15 @@ def calculate_discount(price, discount_percent):
         return final_price
     else:
         return price
+
+original_price = float(input ("Enter original_price:"))
+discount_percentage = float(input("Enter discount percentage:"))
+
+final_price = calculate_discount(original_price, discount_percentage)
+if final_price < original_price:
+    print(f"Final price after discount:{final_price:}")
+else:
+    print(f"No discount applied.")
+    print(f"Final price remains: {original_price}")
+
+# End of Calculator_Discount.py
